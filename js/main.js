1ce850479c16dfcaae5e304bd5f1c24e126d4483
Stephen Higgins <stephenhiggins76@gmail.com>
2016-09-11 19:52:33 +0100

    Add fade in/fade out articles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 function main() {
 
+	$('.about').addClass('article-active')
+
+	// about (home) link
+	$('.nav-links li:first-child').click(function(){
+		var currentArticle = $('.article-active')
+		currentArticle.fadeOut(600).removeClass('article-active')
+		$('.about').fadeIn(800).addClass('article-active')
+	})	
+
+	// portfolio link 
+	$('.nav-links li:nth-child(2)').click(function(){
+		var currentArticle = $('.article-active')
+		currentArticle.fadeOut(600).removeClass('article-active')
+		$('.portfolio').fadeIn(800).addClass('article-active')
+	})
+
+	// contact link
+	$('.nav-links li:nth-child(3)').click(function(){
+		var currentArticle = $('.article-active')
+		currentArticle.fadeOut(600).removeClass('article-active')
+		$('.contact').fadeIn(800).addClass('article-active')
+	})	
+
+
 }
 
 
