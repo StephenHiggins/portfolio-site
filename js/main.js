0cfcedf187b4423e778ee32d26342f51659d88f1
Stephen Higgins <stephenhiggins76@gmail.com>
2016-09-18 11:01:00 +0100

    add code highlighting

--- js/main.js
+++ js/main.js
@@ -1,29 +1,41 @@
 function main() {
 
-window.setInterval(quoteMachine, 5000  ); //thirty seconds
+quoteMachine()
+window.setInterval(quoteMachine, 30000  ); //thirty seconds
 
 	function quoteMachine() {
 		let quoteArr = $('.quoted')
 		// random number generator picks the array element 
 		function getRnd(max) {
 			min = 0
-			
-	    	return Math.floor(Math.random() * (max - min + 1)) + min;
+			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
-		let qL = quoteArr.length -1
-		$('#quote-container>p').replaceWith(quoteArr[getRnd(qL)])
 
+		let qL = quoteArr.length -1
+		let qtIndex = getRnd(qL)
+		let lastQuote
+		if (qtIndex === lastQuote) {
+			qtIndex = getRnd(qL)
+		} 
+		$('#quote-container>p').replaceWith(quoteArr[qtIndex])
+		lastQuote = qtIndex
 		// colour picker
 		let colours = ['#FF4848', '#FF68DD', '#800080', '#872187', '#9A03FE', '#3923D6', '#23819C', '#2966B', '#75ECFD', '#4A9586']
 		let cL = colours.length -1
-		$('#quote-container').parent().css('background-color', colours[getRnd(cL)])
+		let colIndex = getRnd(cL)
+		let lastCol
+		if (colIndex === lastCol) {
+			colIndex = getRnd(cL)
+		}
+		$('#quote-container').parent().css('background-color', colours[colIndex])
 		$('#quote-container>p').css('color', 'white')
-
-
-
-						
+		lastCol = colIndex
 	}
-
 }
 
 $(document).ready(main)
+
+
+
+
+//.css('background-color', colours[getRnd(cL)])
